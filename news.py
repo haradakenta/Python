c91573cecc0114f74d059c156dc26fba2f93b26b
haradakenta <47687311+haradakenta@users.noreply.github.com>
2019-04-08 23:11:33 +0900

    Add files via upload

---
+++ news.py
@@ -0,0 +1,25 @@
+# https://www.yahoo.co.jp/
+# news.yahoo.co.jp/pickup
+
+
+
+
+import requests
+from bs4 import BeautifulSoup
+import re
+
+#初回のみ
+target_url = "https://www.yahoo.co.jp/"
+#Requestsを使って、webから取得
+r = requests.get(target_url)
+#要素を抽出
+soup = BeautifulSoup(r.text, 'lxml')
+
+#HTMLファイルとして保存したい場合はファイルオープンして保存
+with open('originDataOld.html', mode='w', encoding = 'utf-8') as fw:
+    fw.write(soup.prettify())
+
+#soup.find_allを用いてリンク先が「news.yahoo.co.jp/pickup」の項目を全て取得
+elems = soup.find_all(href=re.compile("news.yahoo.co.jp/pickup"))
+for e in elems:
+    print(e.getText())
